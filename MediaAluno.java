2716fffc34a7cab4671a87c7e557ea21879a0f67
kaw90 <kaue_ps@hotmail.com>
2016-09-27 23:46:40 -0300

    Add files via upload

--- MediaAluno.java
+++ MediaAluno.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 
 public class MediaAluno {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		float media,nota1,nota2,nota3;
+		System.out.println("entre com suas notas ");
+		nota1=sc.nextFloat();
+		nota2=sc.nextFloat();
+		nota3=sc.nextFloat();
+		media=(2*nota1+3*nota2+5*nota3)/(2+3+5);
+		System.out.println("sua media é "+ " "+ media);
 		
 	}
 
